f9d48c5af62a6e8d43ac042120ee807b2a50f77f
Grzesiek0301 <g.urbanski@op.pl>
2018-02-03 09:34:02 +0100

    Add possibility to load doctors from file. (#17)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,6 +3,7 @@ package com;
 public class Main {
 	public static void main(String[] args) {
 		DoctorsData doctorsData = new DoctorsData();
+		DoctorListFileReader.loadDoctorsFromFile(doctorsData);
 		Menu menu = new Menu(doctorsData);
 		menu.choice();
 	}
