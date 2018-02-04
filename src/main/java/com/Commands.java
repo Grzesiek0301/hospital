70201e8a3574e94d4d81f6165aa68b787b9a89c8
Grzesiek0301 <g.urbanski@op.pl>
2018-02-04 21:46:14 +0100

    Add validators for adding and removing doctors.

--- src/main/java/com/Commands.java
+++ src/main/java/com/Commands.java
@@ -6,23 +6,25 @@ import java.util.Scanner;
 
 import static com.MedicalSpecialization.getMedicialSpecialization;
 
-public class Commands {
-    public static Doctor getDoctorFromUser() {
+class Commands {
+    static Doctor getDoctorFromUser() {
         String name = getStringParameter("Doctor's name :");
         String surname = getStringParameter("Doctor's surname :");
         int id = getIdFromUser();
-        printMessage("Enter Doctor's medicial specializiation (PATHOLOGY, CARDIOLOGY, UROLOGY, NEUROLOGY):");
-        MedicalSpecialization medicalSpecializiation = getMedicialSpecialization(getStringParameter("Your choise: "));
-        Doctor doctor = new Doctor(name, surname, id, medicalSpecializiation);
-        DoctorFileWriter.writeDoctorDataToFile(doctor);
-        return doctor;
+        printMessage("Enter Doctor's medical specialization (PATHOLOGY, CARDIOLOGY, UROLOGY, NEUROLOGY):");
+        MedicalSpecialization medicalSpecialization = getMedicialSpecialization(getStringParameter("Your choise: "));
+        return new Doctor(name, surname, id, medicalSpecialization);
     }
 
-    public static int getIdToRemove() {
+    static void printMessage(String textOfMessage) {
+        System.out.println(textOfMessage);
+    }
+
+    static int getIdToRemove() {
         return getIdFromUser();
     }
 
-    public static int getIdFromUser() {
+    static int getIdFromUser() {
         System.out.print("Doctor ID: ");
         Scanner readId = new Scanner(System.in);
         return readId.nextInt();
@@ -32,9 +34,5 @@ public class Commands {
         System.out.println(parameter);
         Scanner reader = new Scanner(System.in);
         return reader.nextLine();
-    }
-
-    public static void printMessage(String textOfMessage) {
-        System.out.println(textOfMessage);
     }
 }
--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,10 +1,10 @@
 package com;
 
 public class Main {
-	public static void main(String[] args) {
-		DoctorsData doctorsData = new DoctorsData();
-		DoctorListFileReader.loadDoctorsFromFile(doctorsData);
-		Menu menu = new Menu(doctorsData);
-		menu.choice();
-	}
+    public static void main(String[] args) {
+        DoctorsData doctorsData = new DoctorsData();
+        DoctorListFileReader.loadDoctorsFromFile(doctorsData);
+        Menu menu = new Menu(doctorsData);
+        menu.choice();
+    }
 }
