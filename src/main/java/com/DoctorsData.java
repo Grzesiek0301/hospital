7b80a05ff35f2ed54606ce44b5bda85716f00789
Grzesiek0301 <g.urbanski@op.pl>
2018-07-27 23:01:54 +0200

    Update DoctorsData.java

--- src/main/java/com/DoctorsData.java
+++ src/main/java/com/DoctorsData.java
@@ -8,7 +8,10 @@ import java.util.NoSuchElementException;
 
 public class DoctorsData {
     private final Map<Integer, Doctor> mapDoctors = new HashMap<>();
-
+    
+    public void asdf(){
+    }
+    
     public void addDoctor(int id, Doctor doctor) {
         validateIfDoctorExist(id);
         mapDoctors.put(id, doctor);
